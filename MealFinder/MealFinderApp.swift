9d246b1a8ff3cb2bb46e65554a4b64829242aef7
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-19 12:25:53 -0400

    Initial Commit

---
+++ MealFinder/MealFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFinderApp.swift
+//  MealFinder
+//
+//  Created by Kiho Okazawa on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct MealFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
